7e2682bdf206b5d86650ab9f043f779ad2fc4f91
Daramola Kehinde <daramolakenny18@gmail.com>
2024-10-14 15:00:51 +0100

    my new commit

--- src/script.js
+++ src/script.js
@@ -2,38 +2,42 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
   document.getElementById('mobile-menu').classList.toggle('hidden');
 });
 
-const alertMessageA = document.querySelector('alert-message-a');
-const alertMessageB = document.querySelector('alert-message-b');
-const alertMessageC = document.querySelector('alert-message-c');
-var userName = document.getElementById('name');
-var password = document.getElementById('password');
-var messageBox = document.getElementById('message-box');
 var statusMessage = document.getElementById('status-message');
 
-function submit(){
+function handleSubmit(){
+  if(alertMessageA && alertMessageB && alertMessageC === " ") {
+    statusMessage.style.display = 'block';
+    statusMessage.textContent = "Message Failed.";
+    statusMessage.style.color = "red";
+  }
+  else{
+    statusMessage.style.display = 'block';
+    statusMessage.textContent = "Successfull.";
+    statusMessage.style.color = "red";
+  }
+}  
   // function handleSubmit() {
     
-    if(userName === ''){
-      alertMessageA = 'Name is required';
-      console.log(alertMessageA);
-    }
-    else if(password === ''){
-      alertMessageB = 'Password is required';
-      console.log(alertMessageB);
-    }
-    else if(messageBox === ''){
-      alertMessageC = 'Please enter your message';
-      console.log(alertMessageC);
-    }
-    else{
-      if(statusMessage === 'Successfull'){
-        statusMessage.innerText = 'Successfull';
-        console.log(statusMessage);
-      }
-      else{
-        statusMessage.innerText = 'Failed';
-        console.error(statusMessage);
-      }
-    }
-  // }
-}
+    // if(userName === ''){
+    //   // userName = 'Name is required';
+    //   console.log(alertMessageA);
+    // }
+    // else if(password === ''){
+    //   password = 'Password is required';
+    //   console.log(alertMessageB);
+    // }
+    // else if(messageBox === ''){
+    //   messageBox = 'Please what do you have for me?';
+    //   console.log(alertMessageC);
+    // }
+    // else{
+    //   if(statusMessage === 'Successfull'){
+    //     statusMessage.innerText = 'Successfull';
+    //     console.log(statusMessage);
+    //   }
+    //   else{
+    //     statusMessage.innerText = 'Failed';
+    //     console.error(statusMessage);
+    //   }
+    // }
+// }
